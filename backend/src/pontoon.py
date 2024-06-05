b5221f9834d64e3bca636490259663f10d19ec48
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-06-05 16:38:34 +0100

    Implement Pontoon API with tests

---
+++ backend/src/pontoon.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from src.pontoon_logic import Pontoon
+import uuid
+
+app = FastAPI()
+games = {}
+
+
+class GameID(BaseModel):
+    game_id: str
+
+
+@app.post("/start")
+def start_game():
+    game_id = str(uuid.uuid4())
+    game = Pontoon()
+    game.start_game()
+    games[game_id] = game
+    return {"game_id": game_id, "game_state": game.get_game_state()}
+
+
+@app.post("/hit")
+def hit(game_id: GameID):
+    game = games.get(game_id.game_id)
+    if not game:
+        raise HTTPException(status_code=404, detail="Invalid game_id")
+
+    result = game.player_hit()
+    return {
+        "result": result,
+        "game_state": game.get_game_state()
+    }
+
+
+@app.post("/stand")
+def stand(game_id: GameID):
+    game = games.get(game_id.game_id)
+    if not game:
+        raise HTTPException(status_code=404, detail="Invalid game_id")
+
+    result = game.player_stick()
+    return {
+        "result": result,
+        "game_state": game.get_game_state()
+    }
+
+
+@app.get("/state")
+def state(game_id: str):
+    game = games.get(game_id)
+    if not game:
+        raise HTTPException(status_code=404, detail="Invalid game_id")
+
+    return game.get_game_state()
+
--- backend/tests/test_cards.py
+++ backend/tests/test_cards.py
@@ -1,25 +1,25 @@
-from src.cards import Card, Deck, SUITS, VALUES
+from src.cards import Card, Deck
 import pytest
 
 
 class TestCardClass:
-    def test_invalid_values_raise_key_error(self):
-        with pytest.raises(KeyError):
+    def test_invalid_values_raise_value_error(self):
+        with pytest.raises(ValueError):
             _ = Card("1", "1")
-        with pytest.raises(KeyError):
-            _ = Card("2", "spades")
+        with pytest.raises(ValueError):
+            _ = Card(2, "spades")
 
     def test_card_created_with_suit_and_value(self):
-        ace_of_spades = Card("A", "♠")
+        ace_of_spades = Card(1, 4)
         assert (
-            ace_of_spades.suit == "♠" and
-            ace_of_spades.value == "A"
+            ace_of_spades.get_suit_char() == "♠" and
+            ace_of_spades.get_rank_char() == "A"
         )
 
     def test_integer_rank_available(self):
-        four_of_hearts = Card("4", "♥")
-        ace_of_diamonds = Card("A", "♦")
-        king_of_clubs = Card("K", "♣")
+        four_of_hearts = Card(4, 3)
+        ace_of_diamonds = Card(1, 2)
+        king_of_clubs = Card(13, 1)
         assert (
             four_of_hearts.get_rank() == 4 and
             ace_of_diamonds.get_rank() == 1 and
@@ -27,27 +27,27 @@ class TestCardClass:
         )
 
     def test_integer_rank_returns_14_for_aces_high(self):
-        ace_of_diamonds = Card("A", "♦")
+        ace_of_diamonds = Card(1, 2)
         assert (
             ace_of_diamonds.get_rank(aces_high=True) == 14
         )
   
     def test_alike_cards_are_equal(self):
-        assert Card("A", "♦") == Card("A", "♦")
+        assert Card(1, 2) == Card(1, 2)
 
 
 class TestDeck:
     def test_single_card_deck_has_length_1(self):
-        one_card_deck = Deck(["A"], ["♦"])
+        one_card_deck = Deck([1], [1])
         assert len(one_card_deck) == 1
 
     def test_single_card_deck_deal_method_returns_card(self):
-        one_card_deck = Deck(["A"], ["♦"])
-        assert one_card_deck.pop() == Card("A", "♦")
+        one_card_deck = Deck([1], [1])
+        assert one_card_deck.pop() == Card(1, 1)
         assert len(one_card_deck) == 0
 
-    def test_empty_deck_raises_index_error_on_deal(slef):
-        one_card_deck = Deck(["A"], ["♦"])
+    def test_empty_deck_raises_index_error_on_deal(self):
+        one_card_deck = Deck([1], [1])
         one_card_deck.pop()
         assert len(one_card_deck) == 0
         with pytest.raises(IndexError):
@@ -60,62 +60,65 @@ class TestDeck:
     def test_all_cards_in_default_52_card_deck(self):
         full_deck = Deck()
         card_list = [
-            (card.value, card.suit)
+            (card.rank, card.suit)
             for card in full_deck
             ]
         assert all([
             (v, s) in card_list
-            for v in VALUES
-            for s in SUITS
+            for v in range(1, 14)
+            for s in range(1, 5)
             ])
     
     def test_adding_to_top_of_deck(self):
         test_deck = Deck()
-        card_to_add = Card("4", "♣")
+        card_to_add = Card(4, 1)
         test_deck.append(card_to_add)
         assert len(test_deck) == 53
-        assert test_deck.pop() == Card("4", "♣")
+        assert test_deck.pop() == Card(4, 1)
 
     def test_adding_and_dealing_from_top_and_bottom(self):
-        test_deck = Deck(["5"], ["♦"])
+        test_deck = Deck([5], [2])
         assert len(test_deck) == 1
-        test_deck.append(Card("4", "♣"))
+        test_deck.append(Card(4, 1))
         assert len(test_deck) == 2
-        test_deck.appendleft(Card("2", "♥"))
+        test_deck.appendleft(Card(2, 3))
         assert len(test_deck) == 3
-        assert test_deck.pop() == Card("4", "♣")
-        assert test_deck.popleft() == Card("2", "♥")
+        assert test_deck.pop() == Card(4, 1)
+        assert test_deck.popleft() == Card(2, 3)
+        assert test_deck.pop() == Card(5, 2)
 
     def test_adding_multiple_card_objects_using_extend_method(self):
-        cards = [Card(v, s) for v, s in [("4", "♣"), ("2", "♥"), ("5", "♦")]]
+        cards = [Card(v, s) for v, s in [(4, 1), (2, 3), (5, 2)]]
         three_cards = Deck([])
         three_cards.extend(cards)
         assert len(three_cards) == 3
 
     def test_deck_iteration(self):
-        cards = [Card(v, s) for v, s in [("4", "♣"), ("2", "♥"), ("5", "♦")]]
+        cards = [Card(v, s) for v, s in [(4, 1), (2, 3), (5, 2)]]
         print(cards)
         three_cards = Deck([])
         three_cards.extend(cards)
         iterator = iter(three_cards)
-        assert next(iterator) == Card("4", "♣")
-        assert next(iterator) == Card("2", "♥")
-        assert next(iterator) == Card("5", "♦")
+        assert next(iterator) == Card(4, 1)
+        assert next(iterator) == Card(2, 3)
+        assert next(iterator) == Card(5, 2)
         with pytest.raises(StopIteration):
             next(iterator)
 
     def test_adding_decks(self):
         four_of_clubs = Deck([])
-        four_of_clubs.append(Card("4", "♣"))
+        four_of_clubs.append(Card(4, 1))
+        assert len(four_of_clubs) == 1
         two_of_hearts = Deck([])
-        two_of_hearts.append(Card("2", "♥"))
+        two_of_hearts.append(Card(2, 3))
+        assert len(two_of_hearts) == 1
         two_cards = four_of_clubs + two_of_hearts
         assert len(two_cards) == 2
         assert all(
                 [
                     card in two_cards for card in [
-                        Card("4", "♣"),
-                        Card("2", "♥"),
+                        Card(4, 1),
+                        Card(2, 3),
                     ]
                 ]
             )
