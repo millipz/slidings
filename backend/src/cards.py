9936b137aa2586e23a7a82132f14eb1245924625
Miles Phillips <1217784+millipz@users.noreply.github.com>
2024-06-05 08:38:47 +0100

    Flesh out basic cards and deck

--- backend/src/cards.py
+++ backend/src/cards.py
@@ -1,7 +1,11 @@
 from collections import deque
 
-SUITS: list[str] = tuple("♣ ♦ ♥ ♠".split())
-VALUES: list[str] = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
+SUITS: list[str] = tuple(
+    "♣ ♦ ♥ ♠".split()
+    )
+VALUES: list[str] = tuple(
+    "A 2 3 4 5 6 7 8 9 10 J Q K".split()
+    )
 
 
 class Card:
@@ -34,11 +38,23 @@ class Card:
     def __repr__(self) -> str:
         return f"Card({self.value}{self.suit})"
 
-    def get_rank(self, aces_high: bool = False):
+    def get_rank(self, aces_high: bool = False) -> int:
+        """Returns rank of card, 0 - 14
+
+        Args:
+            aces_high (bool, optional): sets rank
+                for A to 14 if true. Defaults to False.
+
+        Returns:
+            int: rank of card from 0 to 14,
+            joker is 0, high ace is 14
+        """
         try:
             return int(self.value)
         except ValueError:
             match self.value:
+                case "🤡":
+                    return 0
                 case "J":
                     return 11
                 case "Q":
@@ -47,13 +63,42 @@ class Card:
                     return 13
                 case "A":
                     return 14 if aces_high else 1
+                
+    def __eq__(self, other):
+        if self.suit == other.suit and self.value == other.value:
+            return True
+        else:
+            return False
 
 
 class Deck:
+    """
+    A basic playing card deck class
+
+    ...
+
+    Attributes
+    ----------
+    ranks : list[int]
+        a list of the ranks in the deck
+        A - K
+    suits : list[str]
+        a list of the suits in the deck
+        defaults to ["♣", "♦", "♥", "♠"]
+
+    Methods
+    -------
+    get_rank(aces_high = False)
+        Returns an integer representing the rank
+        A = 1, K = 13. A = 14 if aces_high.
+    """
     def __init__(
-        self, suits: list[str] = SUITS, values: list[str] = VALUES, size=1
+        self,
+        values: list[str] = VALUES,
+        suits: list[str] = SUITS,
+        size=1
     ) -> None:
-        self.cards = deque(
+        self._cards = deque(
             [
                 Card(value, suit)
                 for value in values
@@ -61,3 +106,59 @@ class Deck:
                 for _ in range(size)
             ]
         )
+
+    def add(self, card: Card, to_bottom: bool = False):
+        """Add a card to the deck
+
+        Args:
+            card (Card): card to add
+            to_bottom (bool): add to bottom of deck
+                defaults to False
+
+        Returns:
+            None
+        """
+        if not to_bottom:
+            self._cards.append(card)
+        else:
+            self._cards.appendleft(card)
+    
+    def deal(self, from_bottom: bool = False) -> Card:
+        """Deal a card from the top of the deck
+
+        Args:
+            from_bottom (bool): deal from bottom of deck
+                defaults to False
+
+        Returns:
+            Card: the dealt card
+            None: if empty
+        """
+        try:
+            if not from_bottom:
+                return self._cards.pop()
+            else:
+                return self._cards.popleft()
+        except IndexError:
+            return None
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __repr__(self) -> str:
+        return f"Deck({len(self)})"
+
+    def __str__(self):
+        return f'Card deck of size {len(self)})'
+
+    def __getitem__(self, index):
+        return self._cards[index]
+
+    def __eq__(self, other):
+        if len(self) != len(other):
+            return False
+        else:
+            for i in range(0, len(self)):
+                if self._cards[i] != other[i]:
+                    return False
+            return True
--- backend/tests/test_cards.py
+++ backend/tests/test_cards.py
@@ -1,4 +1,4 @@
-from src.cards import Card
+from src.cards import Card, Deck, SUITS, VALUES
 import pytest
 
 
@@ -31,3 +31,56 @@ class TestCardClass:
         assert (
             ace_of_diamonds.get_rank(aces_high=True) == 14
         )
+  
+    def test_alike_cards_are_equal(self):
+        assert Card("A", "♦") == Card("A", "♦")
+
+
+class TestDeck:
+    def test_single_card_deck_has_length_1(self):
+        one_card_deck = Deck(["A"], ["♦"])
+        assert len(one_card_deck) == 1
+
+    def test_single_card_deck_deal_method_returns_card(self):
+        one_card_deck = Deck(["A"], ["♦"])
+        assert one_card_deck.deal() == Card("A", "♦")
+        assert len(one_card_deck) == 0
+
+    def test_empty_deck_returns_none_on_deal(slef):
+        one_card_deck = Deck(["A"], ["♦"])
+        one_card_deck.deal()
+        assert len(one_card_deck) == 0
+        assert one_card_deck.deal() is None
+
+    def test_default_creation_of_52_card_deck(self):
+        full_deck = Deck()
+        assert len(full_deck) == 52
+
+    def test_all_cards_in_default_52_card_deck(self):
+        full_deck = Deck()
+        card_list = [
+            (card.value, card.suit)
+            for card in full_deck
+            ]
+        assert all([
+            (v, s) in card_list
+            for v in VALUES
+            for s in SUITS
+            ])
+    
+    def test_adding_to_top_of_deck(self):
+        test_deck = Deck()
+        card_to_add = Card("4", "♣")
+        test_deck.add(card_to_add)
+        assert len(test_deck) == 53
+        assert test_deck.deal() == Card("4", "♣")
+
+    def test_adding_and_dealing_from_top_and_bottom(self):
+        test_deck = Deck(["5"], ["♦"])
+        assert len(test_deck) == 1
+        test_deck.add(Card("4", "♣"))
+        assert len(test_deck) == 2
+        test_deck.add(Card("2", "♥"), to_bottom=True)
+        assert len(test_deck) == 3
+        assert test_deck.deal() == Card("4", "♣")
+        assert test_deck.deal(from_bottom=True) == Card("2", "♥")
